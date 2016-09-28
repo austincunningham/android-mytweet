6d9d1623987eecbfd80c3699f6ee1578b2149c91
austincunningham <austincunningham@hotmail.com>
2016-09-28 17:11:06 +0100

    MyTweetApp added method for new user

--- app/src/main/java/app/mytweet/app/MyTweetApp.java
+++ app/src/main/java/app/mytweet/app/MyTweetApp.java
@@ -3,10 +3,23 @@ package app.mytweet.app;
 import android.app.Application;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import app.mytweet.models.User;
+
 /**
  * Created by austin on 28/09/2016.
  */
 public class MyTweetApp extends Application {
+
+    public List<User> users = new ArrayList<User>();
+
+    public void newUser(User user){
+        users.add(user);
+        Log.v("MyTweetApp", "new user added");
+    }
+
     @Override
     public void onCreate(){
         super.onCreate();
