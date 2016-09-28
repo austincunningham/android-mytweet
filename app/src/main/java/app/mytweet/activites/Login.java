4b8bc33806a49f04b9dac5ebdcffaf662662b44c
austincunningham <austincunningham@hotmail.com>
2016-09-28 16:22:26 +0100

    login activity login button pressed redirects to mytweet activity

--- app/src/main/java/app/mytweet/activites/Login.java
+++ app/src/main/java/app/mytweet/activites/Login.java
@@ -1,7 +1,9 @@
 package app.mytweet.activites;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import app.mytweet.R;
 
@@ -14,5 +16,12 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+    }
+
+    public void LoginPressed (View view)
+    {
+        startActivity (new Intent(this, MyTweet.class));
+        //Toast toast = Toast.makeText(this, "Login Pressed!", Toast.LENGTH_SHORT);
+        //toast.show();
     }
 }
