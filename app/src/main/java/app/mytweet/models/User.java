b0955377a5cc711a447b407b5c8455a0967fc126
austincunningham <austincunningham@hotmail.com>
2016-09-28 16:45:24 +0100

    User model added

---
+++ app/src/main/java/app/mytweet/models/User.java
@@ -0,0 +1,18 @@
+package app.mytweet.models;
+
+/**
+ * Created by austin on 28/09/2016.
+ */
+public class User {
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String password;
+
+    public User(String firstName,String lastName,String email,String password){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+}
