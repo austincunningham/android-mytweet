8e2554709d399b93b9dddbf5760bb553125f1a90
austincunningham <austincunningham@hotmail.com>
2016-10-01 09:40:55 +0100

    Mytweet date button gets a date

--- app/src/main/java/app/mytweet/activites/MyTweet.java
+++ app/src/main/java/app/mytweet/activites/MyTweet.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.text.Editable;
+import android.widget.TextView;
 
 import app.mytweet.R;
 import app.mytweet.models.Tweet;
@@ -17,18 +19,39 @@ import app.mytweet.models.Tweet;
 public class MyTweet extends AppCompatActivity implements TextWatcher {
 
     private EditText tweetText;
+    private TextView characterCount;
     private Tweet tweet;
-    public int count = 0;
+    public int count = 140;
+    private Button date;
+    private Button tweetButton;
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mytweet);
 
-        tweetText = (EditText) findViewById(R.id.tweetText);
+        tweetButton = (Button)findViewById(R.id.tweetButton);
+        date = (Button)findViewById(R.id.date);
+        tweetText = (EditText)findViewById(R.id.tweetText);
+        characterCount = (TextView)findViewById(R.id.characterCount);
+
         tweet = new Tweet();
 
+        date.setEnabled(false);
+
         tweetText.addTextChangedListener(this);
+
+        if (tweet != null){
+            updateControls(tweet);
+        }
+    }
+
+    public void updateControls(Tweet tweet)
+    {
+        //tweet.setText(tweet.tweet);
+//        String value = count + " ";
+//        characterCount.setText(value);
+        date.setText(tweet.getDateString());
     }
 
     @Override
@@ -43,8 +66,11 @@ public class MyTweet extends AppCompatActivity implements TextWatcher {
     }
     @Override
     public void afterTextChanged(Editable s) {
-        tweet.setTweet(s.toString());
-        count++;
-        Log.v("MyTweetApp", "character count: " + count);
+
+        if (count >= 0){
+            tweet.setTweet(s.toString());
+            count--;
+            Log.v("MyTweetApp", "character count: " + count);
+        }
     }
 }
--- app/src/main/java/app/mytweet/models/Tweet.java
+++ app/src/main/java/app/mytweet/models/Tweet.java
@@ -1,15 +1,37 @@
 package app.mytweet.models;
 
+import java.util.Date;
+import java.util.Random;
+
 /**
  * Created by austin on 30/09/2016.
  */
 public class Tweet {
 
+    public int count;
+    public long date;
+    public Long id;
+
     private String tweet;
+
+    public void Tweet(){
+        id = new Random().nextLong();
+        date = new Date().getTime();
+    }
 
     public void setTweet(String tweet){
         this.tweet = tweet;
     }
 
-    public String getGeolocation() { return tweet;}
+    public String getTweet() { return tweet;}
+
+    public String getDateString() {
+        return "Tweet Date:" + dateString();
+    }
+
+    private String dateString() {
+        String dateFormat = "EEE d MMM yyyy H:mm";
+        return android.text.format.DateFormat.format(dateFormat, date).toString();
+    }
+
 }
