ca8036628abebed2604c2e90dcbc734c8b8499be
austincunningham <austincunningham@oceanfree.net>
2016-10-10 15:52:16 +0100

    TweetListActivity class added

--- app/src/main/java/app/mytweet/activites/TweetListActivity.java
+++ app/src/main/java/app/mytweet/activites/TweetListActivity.java
@@ -1,7 +1,26 @@
 package app.mytweet.activites;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import app.mytweet.R;
+import app.mytweet.app.MyTweetApp;
+import app.mytweet.models.Portfolio;
+
 /**
  * Created by ictskills on 10/10/16.
  */
-public class TweetListActivity {
+public class TweetListActivity extends Activity {
+    private ListView listView;
+    private Portfolio portfolio;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.app_name);
+        setContentView(R.layout.activity_tweetlist);
+        MyTweetApp app = (MyTweetApp)getApplication();
+        portfolio = app.portfolio;
+    }
 }
